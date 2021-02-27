18967dbf83789236882b91fb23fc9870c203f6cd
Devnanda Manjunadh <mdevnanda@gmail.com>
2021-02-27 16:38:51 -0600

    finished app

--- Periodic Elements/ContentView.swift
+++ Periodic Elements/ContentView.swift
@@ -8,9 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var elements = [Element]()
+    @State private var showingAlert = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(elements) { element in
+                NavigationLink(
+                    destination: VStack {
+                        Text(element.name)
+                            .padding()
+                        Text(element.symbol)
+                            .padding()
+                        Text(element.history)
+                            .padding()
+                        Text(element.facts)
+                            .padding()
+                    },
+                    label: {
+                        HStack {
+                            Text(element.symbol)
+                            Text(element.name)
+                        }
+                    })
+            }
+            .navigationTitle("Periodic Elements")
+        }
+        
+        .onAppear(perform: {
+            queryAPI()
+        })
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the data"), dismissButton: .default(Text("OK")))
+        })
+    }
+    
+    func queryAPI() {
+        let apiKey = "?rapidapi-key=f12a856a4bmsh75d13d14144426ep198e21jsn303706aa72b7"
+        let query = "https://periodictable.p.rapidapi.com/\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                let contents = json.arrayValue
+                for item in contents {
+                    let name = item["name"].stringValue
+                    let symbol = item["symbol"].stringValue
+                    let facts = item["facts"].stringValue
+                    let history = item["history"].stringValue
+                    let element = Element(symbol: symbol, name: name, facts: facts, history: history)
+                    elements.append(element)
+                }
+                return
+            }
+        }
+        showingAlert = true
     }
 }
 
@@ -18,4 +69,12 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Element: Identifiable {
+    let id = UUID()
+    let symbol: String
+    let name: String
+    let facts: String
+    let history: String
 }
